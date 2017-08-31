1761afce51f52b2442c2571cca085fb0b750ef6b
Timothy Anthony <pilot.taim@gmail.com>
2017-08-31 14:37:34 +0300

    Implementing drug buying price check

--- app/Http/Controllers/DrugController.php
+++ app/Http/Controllers/DrugController.php
@@ -12,6 +12,67 @@ class DrugController extends Controller
     public function index(Request $request)
     {
         $queryBuilder = new QueryBuilder(new Drug, $request);
+
+        // Buying price checking.
+        if($request->name && $request->buying_price)
+        {
+            $drug = json_decode("{}");
+            $price_check = json_decode("{}");
+            $status = 0;
+
+            if(isset($queryBuilder->build()->get()[0]))
+            {
+                $drug = $queryBuilder->build()->get()[0];
+                $status = 200;
+
+                // Checking
+                $buying_price = str_ireplace(",","",$request->buying_price);
+                $buying_price = str_ireplace("/","",$buying_price);
+                $buying_price = str_ireplace("=","",$buying_price);
+
+                $buying_price = (int)$buying_price;
+
+                $drug_price = str_ireplace(",","",$drug->price);
+                $drug_price = str_ireplace("/","",$drug_price);
+                $drug_price = str_ireplace("=","",$drug_price);
+
+                $drug_price = (int)$drug_price;
+
+                $buying_price_status = "";
+                $extra_amount = 0;
+
+                if($buying_price == $drug_price)
+                {
+                    $buying_price_status = "equal";
+                    $extra_amount = $buying_price - $drug_price;
+                }
+                else if($buying_price > $drug_price)
+                {
+                    $buying_price_status = "above";
+                    $extra_amount = $buying_price - $drug_price;
+                }
+                else if($buying_price < $drug_price)
+                {
+                    $buying_price_status = "below";
+                    $extra_amount = $drug_price - $buying_price;
+                }
+
+                $price_check = array(
+                    'buying_price_status' => $buying_price_status,
+                    'extra_amount' => $extra_amount
+                );
+            }
+            else
+            {
+                $status = 404;
+            }
+
+            return response()->json([
+                'status' => $status,
+                'drug' => $drug,
+                'price_check' => $price_check
+            ],200);
+        }
         
         // Single Drug
         if($request->limit && $request->limit == 1)
