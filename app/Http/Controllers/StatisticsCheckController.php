c9b11f95b918a92971f6f55c845a41211ce440b2
Timothy Anthony <anthony.timothy90@gmail.com>
2017-10-13 13:37:06 +0300

    Adding status 200 when fetching statistics summary

--- app/Http/Controllers/StatisticsCheckController.php
+++ app/Http/Controllers/StatisticsCheckController.php
@@ -75,6 +75,7 @@ class StatisticsCheckController extends Controller
                         }
                         else $sum = 0;
 
+                        $status = 200;
                         $message .= ''.$n.'. '.$keyword->name_long.' - TZS '.$sum.'/=\n';
                         $n++;
                     }
